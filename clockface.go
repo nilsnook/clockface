caafec7441a3d15fc6cc75d74d1c03cf50dbfc1d
Nikhil Sahukar <nsahukar@gmail.com>
2023-01-18 16:29:59 +0530

    Covered acceptance test for second hand at 30 secs

--- clockface.go
+++ clockface.go
@@ -11,10 +11,23 @@ type Point struct {
 	Y float64
 }
 
+const secondHandLength = 90
+const clockCenterX = 150
+const clockCenterY = 150
+
 // SecondHand is the unit vector of the second hand of an analogue clock
 // at time `t` represented as a Point
 func SecondHand(t time.Time) Point {
-	return Point{150, 60}
+	p := SecondHandPoint(t)
+	// scale
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
+	// flip
+	p = Point{p.X, -p.Y}
+	// translate
+	p = Point{p.X + clockCenterX, p.Y + clockCenterY}
+
+	// return Point{150, 60}
+	return p
 }
 
 func SecondsInRadians(t time.Time) float64 {
